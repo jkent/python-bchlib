8d0656ab8f37e734428635501738d360ad80eebd
Jeff Kent <jeff@jkent.net>
2024-09-12 09:18:57 -0500

    Bump minor version for github action fix.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 from setuptools import Extension, setup
 
 NAME = 'bchlib'
-VERSION = '2.1.2'
+VERSION = '2.1.3'
 DESCRIPTION = 'A python wrapper module for the Linux kernel BCH library.'
 URL = 'https://github.com/jkent/python-bchlib'
 EMAIL = 'jeff@jkent.net'
